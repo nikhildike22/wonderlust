bcd25ab0219b7fd987c7d2b6fbcab4fecfb673e3
Nikhil Dike <nikhldike22@gmail.com>
2025-11-28 21:18:41 +0530

    add vercel config

--- app.js
+++ app.js
@@ -140,6 +140,8 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-});
+// app.listen(8080, () => {
+//     console.log('Server is running on port 8080');
+// });
+
+module.exports = app;
